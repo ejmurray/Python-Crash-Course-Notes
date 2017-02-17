b14316c6e1cf59ab5f633e90cdb01e7a8b6cd8f0
Ernest Murray <ejmurray247@hotmail.com>
2017-02-17 09:07:14 +0000

    created the uest book script

--- ex10_3.py
+++ ex10_3.py
@@ -1,1 +1,12 @@
 """Guest book"""
+filename = 'guest_book.txt'
+
+print('Enter "quit" to fininsh!')
+while True:
+    name = input('\nWhat is your name? ')
+    if name == 'quit':
+        break
+    else:
+        with open(filename, 'a') as f:
+            f.write('Guest Name: '+ name + '\n')
+            print('Welcome {}. You have been added to the guest book.'.format(name))
