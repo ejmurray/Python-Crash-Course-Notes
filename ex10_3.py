9558fd584b2093264cd8f91e0d3ec6b8b961d018
Ernest Murray <ejmurray247@gmail.com>
2017-02-19 11:46:21 +0000

    guestbook.py added

--- ex10_3.py
+++ ex10_3.py
@@ -1,1 +1,13 @@
 """Guest book"""
+filename = 'guest_book.txt'
+
+print("Enter 'quit' when you are finished.")
+while True:
+    name = ''
+    name = str(input("\nEnter your name. "))
+    if name == 'quit':
+        break
+    else:
+        with open(filename, 'a') as f:
+            f.write(name + '\n')
+        print("Hi {}, you've been added to the guest book.".format(name))
