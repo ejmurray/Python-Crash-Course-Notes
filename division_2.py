da66bd1b4a1db94de676fe8a95100b3a87c937e1
Ernest Murray <ejmurray247@gmail.com>
2017-02-20 20:13:46 +0000

    error handling

---
+++ division_2.py
@@ -0,0 +1,15 @@
+"""part 2 creating a calculator"""
+
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit")
+
+while True:
+    first_number = input("\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    print(answer)
