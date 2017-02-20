e87664234ec51d4b047c6385267627b2529f23c5
Ernest Murray <ejmurray247@hotmail.com>
2017-02-20 08:36:15 +0000

    removed extra lines

--- ex10_3.py
+++ ex10_3.py
@@ -1,6 +1,7 @@
 """Guest book"""
 
 filename = "guest_book.txt"
+
 print("Enter 'quit' when you are finished.")
 while True:
     name = str(input("\nWhat's your name? "))
@@ -10,5 +11,3 @@ while True:
         with open(filename, 'a') as f:
             f.write('Guest Name: '+ name + '\n')
             print('Welcome {}. You have been added to the guest book.'.format(name))
-            f.write(name + "\n")
-        print("Hi " + name + ", you've been added to the guest book.")
