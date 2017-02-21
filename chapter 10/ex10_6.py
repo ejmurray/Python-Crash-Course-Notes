50b7b9cfbc313dac65f2a6ac66c680b1c013ed28
Ernest Murray <ejmurray247@gmail.com>
2017-02-21 19:49:11 +0000

    ex10_6 updated

--- chapter 10/ex10_6.py
+++ chapter 10/ex10_6.py
@@ -1,25 +1,13 @@
 """add two numbers together and show a TypeError if a non int is entered."""
-print("Give me two numbers, and I'll add them together.")
-print("Enter 'q' to quit")
 
-while True:
-    a = input("\nFirst number: ")
-    if a == 'q':
-        break
-    try:
-        a is int == True
-    except TypeError:
-        print('Please enter an integer.')
-    b = input("Second number: ")
-    if b == 'q':
-        break
-    try:
-        b is int == True
-    except:
-        print('Please enter an integer.')
-    try:
-        answer = int(a) / int(b)
-    except ZeroDivisionError:
-        print("You can't divide by zero!")
-    else:
-        print(answer)
+
+try:
+    a = input('Enter your first number: ')
+    int(a)
+    b = input('Enter a second number: ')
+    int(b)
+except TypeError:
+    print("You need to enter a number!")
+else:
+    sum = int(a) + int(b)
+    print('The sum of {} and {} is {}'.format(a, b, sum))
