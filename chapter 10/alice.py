d73b7282605925e2d232888feab75a5e2f3cc517
Ernest Murray <ejmurray247@hotmail.com>
2017-02-21 15:06:04 +0000

    created function count_words

--- chapter 10/alice.py
+++ chapter 10/alice.py
@@ -1,14 +1,23 @@
 """handling a FileNotFoundError error"""
 
-filename = 'alice.txt'
-try:
-    with open(filename) as f:
-        contents = f.read()
-except FileNotFoundError:
-    msg = "Sorry, the file: {} does not exist.".format(filename)
-    print(msg)
-else:
-    # count the number of words in the filename
-    words = contents.split()
-    num_words = len(words)
-    print("The file: {} has about {} words.".format(filename, str(num_words)))
+def count_words(filename):
+    """Count the approximate number of words in a file."""
+    filename = 'alice.txt'
+    try:
+        with open(filename) as f:
+            contents = f.read()
+    except FileNotFoundError:
+        msg = "Sorry, the file: {} does not exist.".format(filename)
+        print(msg)
+    else:
+        # count the number of words in the filename
+        words = contents.split()
+        num_words = len(words)
+        print("The file: {} has about {} words.".format(filename, str(num_words)))
+
+# filename = "alice.txt"
+# count_words(filename)
+
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_women.txt']
+for filename in filenames:
+    count_words(filename)
