0d161743ce43082434565e01b760908d69d787a6
Ernest Murray <ejmurray247@hotmail.com>
2017-02-22 09:07:06 +0000

    added 10_7.py

--- chapter 10/ex10_2.py
+++ chapter 10/ex10_2.py
@@ -3,7 +3,7 @@ filename = 'learning_python_replace.txt'
 
 with open(filename) as f:
     lines = f.readlines()
-    
+
 for line in lines:
     line = line.strip()
     print(line.replace("Python", "C++"))
