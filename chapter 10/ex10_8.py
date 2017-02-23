0f78f2a50eb153233feebffbb88c435306f273f7
Ernest Murray <ejmurray247@hotmail.com>
2017-02-23 09:21:55 +0000

    updated ex10-8.py

--- chapter 10/ex10_8.py
+++ chapter 10/ex10_8.py
@@ -5,11 +5,13 @@ file is missing.
 filenames = ['cats.txt', 'dogs.txt', 'hamsters.txt']
 
 for filename in filenames:
-    print("\nReading file: {}".format(filename))
     try:
         with open(filename) as f:
             contents = f.read()
-            print(contents)
 
     except FileNotFoundError:
         print("The file named {} is missing".format(filename))
+
+    else:
+        print("\nReading file: {}".format(filename))
+        print(contents)
