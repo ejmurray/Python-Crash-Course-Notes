3d08d055e0f410bf371a72a47d32b9309cdd5810
Ernest Murray <ejmurray247@gmail.com>
2017-03-25 16:48:26 +0000

    added ex4-10.py

--- chapter04/ex04_7.py
+++ chapter04/ex04_7.py
@@ -1,3 +1,3 @@
-# Make a list of multiples of three from 3 to 30
-multiples_of_three = [x*3 for x in range(1,11)]
+"""Make a list of multiples of three from 3 to 30."""
+multiples_of_three = [x * 3 for x in range(1, 11)]
 print(multiples_of_three)
