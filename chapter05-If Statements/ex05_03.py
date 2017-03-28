a652390f3e301aa4963a85156681ce17ac938a5f
Ernest Murray <ejmurray247@hotmail.com>
2017-03-28 16:36:52 +0100

    updated ex05_03.py

--- chapter05-If Statements/ex05_03.py
+++ chapter05-If Statements/ex05_03.py
@@ -2,3 +2,8 @@ alien_colour = 'green'
 
 if alien_colour == 'green':
     print("You have jusr earned 5 points.")
+
+alien_colour = 'red'
+
+if alien_colour == 'green':
+    print("You have jusr earned 5 points.")
