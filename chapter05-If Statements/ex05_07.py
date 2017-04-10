c1ddf26a107c3639409863befb092a4abf43f405
Ernest Murray <ejmurray247@gmail.com>
2017-04-10 16:05:18 +0100

    added ex5-6 and 5-7

---
+++ chapter05-If Statements/ex05_07.py
@@ -0,0 +1,12 @@
+favourite_fruit = ['mango', 'banana', 'apple']
+
+if 'orange' in favourite_fruit:
+    print("\nOranges are nice frshly squeezed!")
+if "mango" in favourite_fruit:
+    print("\nMango is in the list of my favourite fruit.")
+if "lemon" in favourite_fruit:
+    print("\nLemons are a bit sour, but nice." + '\n')
+if "banana" in favourite_fruit:
+    print("\nBananas are a good source of energy.")
+if "apple" in favourite_fruit:
+    print("\nYou can't beat eating an apple.")
