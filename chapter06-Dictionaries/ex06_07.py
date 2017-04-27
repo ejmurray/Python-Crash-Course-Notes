cfa12921dcb27de231576a59d88ad6732cab4c4c
Ernest Murray <ejmurray247@gmail.com>
2017-04-27 14:27:11 +0100

    updated exercises

--- chapter06-Dictionaries/ex06_07.py
+++ chapter06-Dictionaries/ex06_07.py
@@ -7,7 +7,7 @@ person_id_1 = {
 
 person_id_2 = {
     'first_name': 'Busta',
-    'last_name': 'Smith',
+    'last_name': 'Rhymes',
     'age': 24,
     'city': 'New York',
 }
@@ -23,4 +23,4 @@ people = [person_id_1, person_id_2, person_id_3]
 
 for person in people:
     for k, v in person.items():
-        print("\n{}: {}".format(k.title(), v))
+        print("{}: {}".format(k.title(), v))
