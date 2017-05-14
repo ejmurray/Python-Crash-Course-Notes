7191562f6eab150eaaec1478c3cf74243ea0c41a
Ernest Murray <ejmurray247@gmail.com>
2017-05-14 15:37:41 +0100

    added chapter 8

---
+++ chapter08-Functions/ex08_01.py
@@ -0,0 +1,5 @@
+def display_message():
+    """Take a message and display it as output"""
+    print("I'm learning python and data science as part of my day job!")
+
+display_message()
---
+++ chapter08-Functions/greeter.py
@@ -0,0 +1,6 @@
+def greet_user(username):
+    """Display a simple greeting."""
+    print("Hello, {}!".format(username.title()))
+
+greet_user('jesse')
+greet_user('jamal')
